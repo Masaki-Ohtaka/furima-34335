fa8cea0e25a1ce44ebab2eac4ed177bf7c79608e
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-04-25 15:51:02 +0900

    ヘッダーのログイン、新規登録ボタンから各ページに遷移設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_paramters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 private
-  def confiqure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname,:last_name,:first_name,:last_kana,:first_kana,:birthday])
   end
 end
