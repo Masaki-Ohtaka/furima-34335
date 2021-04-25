ac725370a55967159a9a1e5a95bb948d478ebadd
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-04-25 15:25:28 +0900

    トップページの表示設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_paramters, if: :dvise_controller?
+  before_action :configure_permitted_paramters, if: :devise_controller?
 
-  private
+private
   def confiqure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname,:last_name,:first_name,:last_kana,:first_kana,:birthday])
   end
--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,4 +1,7 @@
+require 'test_helper'
+
 class ItemsControllerTest < ActionDispatch::IntegrationTest
-  def index
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
