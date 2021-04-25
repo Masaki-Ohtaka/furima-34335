6e12a8d86e26844b6847335035dd404dade1bbdd
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-04-26 06:27:02 +0900

    バリデーション修正〜ユーザー名に全角（漢字・ひらがな・カタカナ)、ユーザー名のフリガナは全角（カタカナ）入力制限をかける

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,presence: true 
-  validates :last_name,presence: true
-  validates :first_name,presence: true
-  validates :last_kana,presence: true
-                     # format: {
-                     #   with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
-                     # }
-  validates :first_kana,presence: true
-                     # format: {
-                     #   with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
-                     # }
+  with_options presence: true do
+    # ひらがな、カタカナ、漢字のみ入力出来る
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    # ひらがな、カタカナ、漢字のみ入力出来る
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    # カタカナのみ入力出来る
+    validates :last_kana,format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    # カタカナのみ入力出来る
+    validates :first_kana,format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  end
   validates :birthday,presence: true 
   
   has_many :itemes
