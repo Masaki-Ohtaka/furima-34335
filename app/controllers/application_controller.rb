2634facc3d15f639c54ff53a61e59fef3fa1db37
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-04-25 09:20:30 +0900

    ストロングパラメーターの設定、バリデーション、READOMEの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramters, if: :dvise_controller?
+
+  private
+  def confiqure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname,:last_name,:first_name,:last_kana,:first_kana,:birthday])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,presence: true
-  validates :email,presence: true
-  validates :encrypted_password,presence: true
+  validates :nickname,presence: true 
   validates :last_name,presence: true
   validates :first_name,presence: true
   validates :last_kana,presence: true
