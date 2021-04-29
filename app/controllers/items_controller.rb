bbcbebbba116b463962145c5979ae4dbe39cff6b
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-04-30 07:25:19 +0900

    コントローラー編集〜出品機能保存処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Iteme.create(item_params)
+  end
+  
+  private
+  def item_params
+    #アクティブハッシュのハッシュ呼び出しが理解していないので要確認
+    params.require(:item).permit(:image, :title, :item_description, :category_id, :shipping_change_id, :days_to_ship_id, :price)
+  end
 end
