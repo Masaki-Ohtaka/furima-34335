27688620c4a44b82b6e5a7da3e3ce86190c477b4
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-04-30 16:50:27 +0900

    itemモデル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
   
   private
   def item_params
-    #アクティブハッシュのハッシュ呼び出しが理解していないので要確認
+    #ActiveHashからHash呼び出しが理解していないので要確認
     params.require(:item).permit(:image, :title, :item_description, :category_id, :shipping_change_id, :days_to_ship_id, :price)
   end
 end
