03a657a93feabcdadb2c6b9a24c0678124d29982
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-05-01 12:20:43 +0900

    ストロングパラメーター編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,6 @@ class ItemsController < ApplicationController
   private
   def item_params
     #ActiveHashからHash呼び出しが理解していないので要確認
-    params.require(:item).permit(:image, :title, :item_description, :category_id, :shipping_change_id, :days_to_ship_id, :price)
+    params.require(:item).permit(:image, :title, :item_description, :category_id, :item_condition_id, :shipping_change_id, :shipping_area_id, :days_to_ship_id, :price)
   end
 end
