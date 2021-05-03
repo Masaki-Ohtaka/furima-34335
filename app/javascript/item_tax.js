ae8359398435b3177b77b07fdc47651ec0507837
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-05-03 22:30:57 +0900

    JavaScript処理、レスポンス速度向上させる為編集

--- app/javascript/item_tax.js
+++ app/javascript/item_tax.js
@@ -1,5 +1,6 @@
 function item_tax (){
   const itemPrice  = document.getElementById("item-price");
+  if (!itemPrice){ return false;}
   itemPrice.addEventListener("keyup", () => {
     const fee = Math.floor(itemPrice.value * 0.1);
     const addTaxPrice = document.getElementById("add-tax-price");
