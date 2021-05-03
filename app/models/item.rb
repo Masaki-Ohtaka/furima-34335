b7fdf59a4be053f95cd7f820430081dbefbf20d8
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-05-03 09:35:02 +0900

    バリデーション・アソシエーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,20 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_change
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+  
+  belongs_to :user, optional: true
+  has_one    :purchase
+  has_one_attached :image 
+
   with_options presence: true do
     validates :image
     validates :title
     validates :item_description
-    validates :price
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
   end
   with_options numericality: { other_than:1} do
   validates :category_id
@@ -13,14 +23,5 @@ class Item < ApplicationRecord
   validates :shipping_area_id
   validates :days_to_ship_id
   end
-
-  belongs_to :user
-  has_one    :purchase
-  has_one_attached :image  
-  belongs_to :category
-  belongs_to :item_condition
-  belongs_to :shipping_change
-  belongs_to :shipping_area
-  belongs_to :days_to_ship
-
+   
 end
