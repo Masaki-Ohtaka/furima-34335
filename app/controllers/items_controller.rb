8e68f90b0a3755963a37d378f9da3f226f1e4d9a
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-05-05 17:08:09 +0900

    アソシエーションの設定、コントローラー処理の編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_item, only: [:edit, :update, :show]
 
   def index
@@ -21,8 +21,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Prototype.find(params[:id])
-    ite.destroy
+    item = Item.find(params[:id])
+    item.destroy
     redirect_to root_path
   end
 
