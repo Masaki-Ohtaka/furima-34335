c580687e75406ea965c5ce09b14075db24a82c2c
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-05-05 17:27:40 +0900

    private内で定義しているset_item用いた仕様に修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:destroy, :edit, :update, :show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -21,8 +21,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
