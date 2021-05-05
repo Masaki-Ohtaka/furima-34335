9b45e47114680947d8c6726ed5d0149519aee799
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-05-05 16:42:37 +0900

    ルーティング、コントローラーに削除処理記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Prototype.find(params[:id])
+    ite.destroy
+    redirect_to root_path
+  end
+
   def edit
     unless @item.user_id == current_user.id 
       redirect_to root_path
