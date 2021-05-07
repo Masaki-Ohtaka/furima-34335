c98c6bb1ffcbca02fd9113570a483e2ffed6481c
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-05-08 07:57:37 +0900

    購入、住所のアソシエーション・マイグレーションファイル設定

--- db/migrate/20210507223332_create_addresses.rb
+++ db/migrate/20210507223332_create_addresses.rb
@@ -1,6 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
+      t.string     :postal_code,      null: false
+      t.integer    :location_id,      null: false
+      t.string     :municipality,     null: false
+      t.string     :address,          null: false       
+      t.string     :building_name
+      t.references :purchases,        null: false, foreign_key: true
 
       t.timestamps
     end
