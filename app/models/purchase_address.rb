cbbd48d95790ec61d84ea82c303de57ab43aa18b
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-05-09 05:07:46 +0900

    Formオブジェクトに用いるモデルにバリデーション編集

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,16 +1,15 @@
 class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id,  :postal_code, :location_id, :municipality, :address, :building_name, :phone_number
-  with_options presence: true do
-    validates :user_id
-    validates :item_id
-    validates :location_id
-    validates :municipality
-    validates :address
-    validates :phone_number
-    # 数字3桁、ハイフン、数字4桁の並びのみ許可する
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  end
+    with_options presence: true do
+      validates :user_id
+      validates :item_id
+      validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Include hyphen(-)' }# 数字3桁、ハイフン、数字4桁の並びのみ許可する
+      validates :location_id, numericality: { other_than:1} 
+      validates :municipality
+      validates :address
+      validates :phone_number, format: { with: /\A0[0-9]+\z/, message: 'number is invalid. Include half-width numbers' } 
+    end
 
   def save
     # 購入情報を保存し、変数puchasに代入する要確認
