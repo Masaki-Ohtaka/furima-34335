b0ab1dd5398ebb8d367a4a7c828b66eaa8a4c9bd
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-05-08 12:16:15 +0900

    ストロングパラメーター設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,5 +4,18 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :location_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id)
   end
 end
