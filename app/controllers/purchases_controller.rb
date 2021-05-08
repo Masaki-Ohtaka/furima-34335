1f1b9f167d8c7b0814ed3966341fd257021b1ab9
Masaki-Ohtaka <tixeko1@yahoo.co.jp>
2021-05-08 12:40:39 +0900

    ページ遷移制限

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,10 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   def index
     @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def create
